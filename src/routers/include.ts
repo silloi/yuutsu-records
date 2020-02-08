a6faf9c74b754e86962cae958cd696d6881f2d35
silloi <silloi1993@gmail.com>
2020-02-08 11:58:26 +0900

    fix: cancel importing include.vue

--- src/routers/include.ts
+++ src/routers/include.ts
@@ -1,11 +1,11 @@
-import INuxtRoute from '../interfaces/app/INuxtRoute'
+// import INuxtRoute from '../interfaces/app/INuxtRoute'
 
-export default function(
-  resolve: (dirname: string, routeDir: string) => string
-): INuxtRoute {
-  return {
-    name: 'include',
-    path: '/include',
-    component: resolve(__dirname, '../include/include.vue')
-  }
-}
+// export default function(
+//   resolve: (dirname: string, routeDir: string) => string
+// ): INuxtRoute {
+//   return {
+//     name: 'include',
+//     path: '/include',
+//     component: resolve(__dirname, '../include/include.vue')
+//   }
+// }
